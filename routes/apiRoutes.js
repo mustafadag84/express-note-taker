5c5308022a5eae333a5bc207873d7c89b1a7f1ad
Mustafa Dag <mustafaa_dag@hotmail.com>
2020-08-17 14:31:37 -0400

    DELETE API route fixed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,18 @@
-// ===============================================================================
-// LOAD DATA
+
 // We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
+// These data sources hold arrays of information
+
 
 var notesData = require("../db/db.json");
 console.log(notesData);
 var fs = require("fs");
 // ===============================================================================
+
 // ROUTING
-// ===============================================================================
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
+  // API GET Requests  
   app.get("/api/notes", function(req, res) {
     res.json(notesData);
   });
@@ -32,27 +27,19 @@ module.exports = function(app) {
     res.json(notesData);
   });
 
+ // remove notes from notesData with id 
   app.delete("/api/notes/:id", function(req, res) {
     let noteId = req.params.id
     let newNote = [];
-    // remove notes from notesData with id 
     for (let i = 0; i < notesData.length; i++) {
       if (notesData[i].id != noteId){
-        newNote.push(noteId[i])
+        newNote.push(notesData[i])
       }
     }
     fs.writeFile("./db/db.json",JSON.stringify(newNote),function(err){console.log("Deleted Note")});
     notesData = newNote;
     res.json(notesData);
   });
-
-  
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
 
   
 };
